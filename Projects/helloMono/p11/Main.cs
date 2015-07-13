a8893ab43809cb68b3c8a05a6c2102c768f580fc
Kyohei Saito <saihe.kyon@gmail.com>
2015-07-13 15:31:06 +0900

    ７月１３日追加。

---
+++ Projects/helloMono/p11/Main.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace p11
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("練習(11)--------------------------------");
+			int i = 0;
+			int[] testAges = new int[20];
+			while(i < 20){
+				testAges[i] = i;
+				i++;
+			}
+			foreach (var item in testAges) {
+				if(item >= 7 && item <= 12){
+					Console.WriteLine(item + "小学生");
+				}else if(item >= 13 && item <= 15){
+					Console.WriteLine(item + "中学生");
+				}else{
+					Console.WriteLine(item);
+				}
+				var input = Console.ReadLine();
+				int inputInt = int.Parse(input);
+				if(inputInt > 10){
+					Console.WriteLine("でかい！！");
+				}else{
+					Console.WriteLine("小さい");
+				}
+			}
+
+		}
+	}
+}
---
+++ Projects/helloMono/p12/Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace p12
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var age Console.Read ();
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
